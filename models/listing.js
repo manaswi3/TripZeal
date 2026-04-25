a81a42a9bfb1fb7f91395b12dff4289f71167edf
Manaswi Saxena <manaswisaxena3@gmail.com>
2026-04-25 23:52:19 +0530

    add review deletion feature using pull operator and cascade middleware

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review");
 
 const listingSchema = new Schema({
     title:{
@@ -27,6 +28,16 @@ const listingSchema = new Schema({
             ref:"Review"
         }
     ]
+});
+
+//Middleware for the case
+//------If a listing got deleted then all reviews must be deletd from the db-------
+listingSchema.post("findByIdandDelete",async(listing)=>{
+    if(listing){
+        await Review.deleteMany(
+            {_id : { $in: listing.reviews }}
+        );
+    }
 });
 
 const Listing = mongoose.model("Listing",listingSchema);
