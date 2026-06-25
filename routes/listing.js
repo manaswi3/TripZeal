11e62e53504cb41f9c4e372c3c380f087439f137
Manaswi Saxena <manaswisaxena3@gmail.com>
2026-06-25 23:55:52 +0530

    Connected login route(Auth1)

--- routes/listing.js
+++ routes/listing.js
@@ -4,6 +4,7 @@ const wrapAsync = require("../utils/wrapAsync.js")
 const ExpressError = require("../utils/ExpressError.js")
 const { listingSchema} = require("../schema_valid.js")
 const Listing = require("../models/listing.js");
+const {isLoggedIn} = require("../middleware.js");
 
 
 const validateListing = (req,res,next)=>{
@@ -28,11 +29,11 @@ router.get("/",wrapAsync(async(req,res)=>{
 }));
 
 //Create Route
-router.get("/new",(req,res)=>{
+router.get("/new", isLoggedIn, (req,res)=>{
     res.render("listings/new.ejs");
 })
 
-router.post("/",validateListing, wrapAsync(async (req,res,next)=>{
+router.post("/", isLoggedIn, validateListing, wrapAsync(async (req,res,next)=>{
     // if valid listing is not sent
     // if(!req.body.listing){
     //     throw new ExpressError(400,"Send valid data for listing");
@@ -46,7 +47,7 @@ router.post("/",validateListing, wrapAsync(async (req,res,next)=>{
 }))
 
 //update Route
-router.get("/:id/edit",wrapAsync(async(req,res) =>{
+router.get("/:id/edit", isLoggedIn, wrapAsync(async(req,res) =>{
     let {id}=req.params;
     const data = await Listing.findById(id);
     if(!data){
@@ -56,7 +57,7 @@ router.get("/:id/edit",wrapAsync(async(req,res) =>{
     res.render("listings/edit.ejs",{data})
 }))
 
-router.put("/:id",validateListing, wrapAsync(async (req,res)=>{
+router.put("/:id", isLoggedIn, validateListing, wrapAsync(async (req,res)=>{
     let {id}=req.params;
     //tod ke saare feilds me updated value daal di
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
@@ -77,7 +78,7 @@ router.get("/:id",wrapAsync(async(req,res)=>{
 }));
 
 //Delete Route
-router.delete("/:id",wrapAsync(async(req,res)=>{
+router.delete("/:id", isLoggedIn, wrapAsync(async(req,res)=>{
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
