e7533ccd90bdb797482a6d1cd12988c2d06e65b1
Manaswi Saxena <manaswisaxena3@gmail.com>
2026-06-25 23:02:52 +0530

    Authentication setup through passport

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js")
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js")
 
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 const sessionOptions = {
     secret:"mysupersecret",
@@ -26,9 +30,14 @@ const sessionOptions = {
 
 app.use(session(sessionOptions));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use(express.static(path.join(__dirname,"/public")))
-
-
 app.engine('ejs',ejsMate);
 app.use(express.urlencoded({extended:true}))
 app.set("view engine","ejs");
@@ -41,7 +50,15 @@ app.use((req,res,next)=>{
     next();
 })
 
+// app.get("/demouser",async (req,res)=>{
+//     let fakeUser =new User({
+//         email:"demo@gmail.com",
+//         username: "student_demo",
+//     });
 
+//     let userDemo = await User.register(fakeUser,"helloworld");
+//     res.send(userDemo);
+// });
 
 main().then(()=>{
     console.log("connect to DB");
@@ -56,11 +73,14 @@ app.get("/",(req,res)=>{
 })
 
 //All Listings Route----->>
-app.use("/listings",listings);
+app.use("/listings",listingRouter);
 
 
 //All Reviews Route----->>
-app.use("/listings/:id/reviews",reviews);
+app.use("/listings/:id/reviews",reviewRouter);
+
+//All user Route-------->>
+app.use("/",userRouter);
 
 
 //as per the newer version of express , (*)-->(a wildcard lib) is not valid now
