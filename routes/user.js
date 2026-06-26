b335f6f86d7fb67eec8559b21078637dc35dcbe2
Manaswi Saxena <manaswisaxena3@gmail.com>
2026-06-26 23:56:46 +0530

    Logout User functionality

--- routes/user.js
+++ routes/user.js
@@ -31,6 +31,16 @@ router.get("/login",(req,res)=>{
 router.post("/login",passport.authenticate("local",{failureRedirect:"/login",failureFlash:true}),async(req,res)=>{
 req.flash("success","Welcome back to TripZeal");
 res.redirect("/listings");
-})
+});
+
+router.get("/logout",(req, res, next)=>{
+    req.logout((err)=>{
+        if(err){
+            return next(err);
+        }
+        req.flash("success","You are logged out now!");
+        return res.redirect("/listings");
+    });
+});
 
 module.exports = router;
