023d19b8d1d490193b72aa73db8472c47a781fba
Manaswi Saxena <manaswisaxena3@gmail.com>
2026-07-01 00:00:38 +0530

    Post-Login page  functionality

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
-const passport = require("passport")
+const passport = require("passport");
+const {saveRedirectUrl} = require("../middleware.js");
 
 router.get("/signup",(req,res)=>{
     res.render("users/signup.ejs");
@@ -34,9 +35,10 @@ router.get("/login",(req,res)=>{
     res.render("users/login.ejs");
 });
 
-router.post("/login",passport.authenticate("local",{failureRedirect:"/login",failureFlash:true}),async(req,res)=>{
+router.post("/login",saveRedirectUrl, passport.authenticate("local",{failureRedirect:"/login",failureFlash:true}),async(req,res)=>{
 req.flash("success","Welcome back to TripZeal");
-res.redirect("/listings");
+let redirectUrl = res.locals.redirectUrl||"/listings";
+res.redirect(redirectUrl);
 });
 
 router.get("/logout",(req, res, next)=>{
