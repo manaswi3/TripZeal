8c5e66c9d94cb9c2b4ceab9ee8ad160dab11788b
Manaswi Saxena <manaswisaxena3@gmail.com>
2026-08-04 23:47:52 +0530

    updated the schema to store url of image in cloud

--- models/listing.js
+++ models/listing.js
@@ -12,10 +12,7 @@ const listingSchema = new Schema({
         // type:String,
         // default:"https://images.unsplash.com/photo-1603262110263-fb0112e7cc33?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8amFpcHVyfGVufDB8fDB8fHww",
         filename: String,
-        url: {
-        type: String,
-        default: "https://images.unsplash.com/photo-1603262110263-fb0112e7cc33?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8amFpcHVyfGVufDB8fDB8fHww"
-    }
+        url: String,
         // set:(v)=> v===""?"https://images.unsplash.com/photo-1603262110263-fb0112e7cc33?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8amFpcHVyfGVufDB8fDB8fHww":v,
     },
     price: Number,
