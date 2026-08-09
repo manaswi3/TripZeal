b348d03a264de03a90f267baec93b8cf202ed603
Manaswi Saxena <manaswisaxena3@gmail.com>
2026-08-09 22:51:33 +0530

    Map marker display according to coords

--- public/js/map.js
+++ public/js/map.js
@@ -1,32 +1,15 @@
 import * as maplibregl from "https://unpkg.com/maplibre-gl@6.2.0/dist/maplibre-gl.mjs";
-async function getCoordinates() {
-    const location = cc;
-
-    const response = await fetch(
-        `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(location)}&format=json&limit=1`,
-        {
-            headers: {
-                "User-Agent": "TripZeal"
-            }
-        }
-    );
-
-    const data = await response.json();
-    return [data[0].lon,data[0].lat];
-}
-
-const coordinates = await getCoordinates();
-console.log(coordinates);
+console.log(cc);
 
 const map = new maplibregl.Map({
     container: "map",
     style: "https://tiles.openfreemap.org/styles/bright",
-    center: coordinates,
+    center: cc,
     zoom: 14,
 });
 
 new maplibregl.Marker()
-    .setLngLat(coordinates)
+    .setLngLat(cc)
     .addTo(map);
 
 map.addControl(new maplibregl.NavigationControl());
